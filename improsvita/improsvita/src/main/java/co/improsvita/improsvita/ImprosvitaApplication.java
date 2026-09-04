893ef03937dce152aaae7bd6de7fb9b31ff1ca3b
William González <ruyk1120@gmail.com>
2026-09-03 21:29:18 -0500

    Initial Structure of project generated with Spring initializr

---
+++ improsvita/improsvita/src/main/java/co/improsvita/improsvita/ImprosvitaApplication.java
@@ -0,0 +1,13 @@
+package co.improsvita.improsvita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprosvitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImprosvitaApplication.class, args);
+	}
+
+}
---
+++ improsvita/improsvita/src/test/java/co/improsvita/improsvita/ImprosvitaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.improsvita.improsvita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprosvitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
